d2baef732acdfe3ee336c92f6e6c77eaeb26b220
Paul Vudmaska <paul@paul-vudmaskas-macbook.local>
2009-06-27 12:21:17 -0500

    default message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  protected
+  def twilio_connect
+    require 'net/http'
+    uri = URI.parse("#{TWILIO.url}/#{TWILIO.sid}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req.basic_auth TWILIO.sid, TWILIO.token
+    response = http.request(req)
+    logger.info response.body
+  end
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
