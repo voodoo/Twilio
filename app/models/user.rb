a07e8cd7cfc5cb4ce5427374e75886d2a1fa37f7
Paul Vudmaska <paul@paul-vudmaskas-macbook.local>
2009-07-12 10:46:12 -0500

    default message

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ActiveRecord::Base
   attr_accessor :password
   before_save :prepare_password
   
+  # take out chars from mask
+  def before_validation
+    self.phone.gsub!(/\D/,'')
+  end
+    
   validates_presence_of :username
   validates_uniqueness_of :username, :email, :phone, :allow_blank => true
   validates_format_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
